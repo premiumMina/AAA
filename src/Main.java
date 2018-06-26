9a80c334680000d7c2647ec2b1389b2335e3a0fd
premiumMina <premiumMina@gmail.com>
2018-06-26 15:02:51 +0900

    ming_ 네번째커밋

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("dev_ming에서 세번째 작업내용입니다.");
+        System.out.println("dev_ming에서 네번째 작업내용입니다.");
     }
 }
