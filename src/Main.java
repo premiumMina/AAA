6be87ff52af4a355c0a60b1cab585a22917143f9
premiumMina <premiumMina@gmail.com>
2018-06-26 14:54:34 +0900

    ming _ 첫번째커밋

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("마스터에서 두번째 작업내용입니다.");
+        System.out.println("dev_ming에서 첫번째 작업내용입니다.");
     }
 }
