e12e82760adc7e88c9837926fe470e50f8169c22
premiumMina <premiumMina@gmail.com>
2018-06-26 15:07:25 +0900

    cheng_ 두번째커밋

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("dev_cheng에서 첫번째 작업내용입니다.");
+        System.out.println("dev_cheng에서 두번째 작업내용입니다.");
     }
 }
