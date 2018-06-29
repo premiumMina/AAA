4353d8d3b816af5bc936df8197e0920f3c3e85f4
premiumMina <premiumMina@gmail.com>
2018-06-29 22:42:39 +0900

    teacher_첫번째커밋

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("dev_ming에서 네번째 작업내용입니다.");
+        System.out.println("dev_teacher 첫번째 수정내용입니다.");
     }
 }
